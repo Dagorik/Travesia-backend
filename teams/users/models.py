b1a0a1e376fd1dcd0feac7b789435ef6f75d45db
Edwin Salgado <edwin@devf.mx>
2018-12-03 12:02:12 -0600

    changes model

--- teams/users/models.py
+++ teams/users/models.py
@@ -9,5 +9,16 @@ class User(AbstractUser):
     phone = models.CharField(max_length=12,null=True)
     is_leader =  models.BooleanField(default=False)
     profile_pic = models.URLField()
+    name_group_scout = model.CharField(max_length=250,blank=True, null=True)
+    clan_name =  model.CharField(max_length=250,blank=True, null=True)
+    province =  model.CharField(max_lengt=250,blank=True, null=True)
+    state =  model.CharField(max_length=250,blank=True, null=True)
+    blod_type =  models.CharField(max_length=150,blank=True, null=True)
+    allergies = models.CharField(max_length=250,blank=True, null=True)
+    ailment = models.TextField(blank=True, null=True)
+    special_medicine =  models.TextField(blank=True, null=True)
+    emergency_contact_name =  models.CharField(max_length=150,blank=True, null=True)
+    emergency_contact_phone =  model.CharField(max_length=150,blank=True, null=True)
+
 
 
