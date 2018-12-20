15b4b6412bf4ce30195440895ef0ec0ba4dd992c
Edwin Salgado <edwin@devf.mx>
2018-12-20 16:30:59 -0600

    changes for baucher

--- teams/users/models.py
+++ teams/users/models.py
@@ -24,6 +24,7 @@ class User(AbstractUser):
     emergency_contact_name =  models.CharField(max_length=150,blank=True, null=True)
     emergency_contact_phone =  models.CharField(max_length=150,blank=True, null=True)
     baucher =  models.URLField(blank=True, null=True)
+    valid_baucher =  models.BooleanField(default=False)
 
 def generate_codes():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
